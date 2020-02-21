3e1aebfb3d77760bcfdb08c403026cd652df378c
Roby Baiaturridwan Suryana <rb.aturridwan@gmail.com>
2020-02-21 16:18:23 +0700

    add post method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManagerStatic as Image;
 class ProductController extends Controller
 {
     //
@@ -22,16 +25,45 @@ class ProductController extends Controller
         //return $r->json()->all();
         //return $r->all();
         //Product::create($r->json()->all());
+        $image = $r->productImage? $r->productImage : null;
+        if($image == null){
+            $data = [
+                "productName"=>$r->productName,
+                "productSlug"=>Str::slug($r->productName),
+                "productQty"=>$r->productQty,
+                "productImage"=>null,
+                "categoryId"=>$r->categoryId,
+                "merchantId"=>$r->merchantId
+            ];
+            $response = [
+                "image"=> $image
+            ];
 
-        $data = [
-            "productName",
-            "productSlug",
-            "productQty",
-            "productImage",
-            "categoryId",
-            "merchantId"
-        ];
+        }
+        else{
+            $image = $r->productImage;  // your base64 encodedph
+            preg_match("/data:image\/(.*?);/",$image,$image_extension); // extract the image extension
+            $image = preg_replace('/data:image\/(.*?);base64,/','',$image); // remove the type part
+            $image = str_replace(' ', '+', $image);
+            $imageName = 'image_' . time() . '.' . 'png'; //generating unique file name;
+            Storage::disk('public')->put('images/products/'.$imageName,base64_decode($image));
+            $response = [
+                "image"=>$imageName
+            ];
+            $data = [
+                "productName"=>$r->productName,
+                "productSlug"=>Str::slug($r->productName),
+                "productQty"=>$r->productQty,
+                "productImage"=>'/images/products/'.$imageName,
+                "categoryId"=>$r->categoryId,
+                "merchantId"=>$r->merchantId
+            ];
+        }
+        //if($r->productImage)
 
-        return $data;
+
+        Product::create($data);
+
+        return response()->json($response,200);
     }
 }
