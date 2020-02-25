677df14b85a987d850e10695eab931b21963df8f
Roby Baiaturridwan Suryana <rb.aturridwan@gmail.com>
2020-02-25 17:40:07 +0700

    change route delete product

--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,20 @@ Route::get("/tes", function () {
     return App\Product::all();
 });
 
+// get all product
 Route::get('/products','ProductController@getAllProducts')->name('products');
+
+//get product by slug
 Route::get('/product/{slug}','ProductController@getProductBySlug')->name('product.by.slug');
+
+//get product by id
 Route::get('/product-by-id/{id}','ProductController@getProductById')->name('product.by.id');
 
+//post product
 Route::post('/products','ProductController@saveProduct')->name('products.save');
 
+//get all categories
 Route::get('/categories','CategoriesController@getAllCategories');
 
 //hapus product
-Route::delete('/product/{id}', 'ProductController@deleteProductById');
+Route::delete('/product/{id}/delete', 'ProductController@deleteProductById');
